abcaaf7c88b0eb1a4d8b55b4de97c51aa569817f
Gabriel Lemire <gabriel.lemire@gmail.com>
2017-04-08 22:53:27 -0400

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,11 @@
-if [ ${TRAVIS_TAG} ] && [ "${TRAVIS_BRANCH}" = "master" ] && [ ${NPM_TOKEN} ]
+if [ ${TRAVIS_TAG} ] && [ ${NPM_TOKEN} ]
 then
   docker cp robs-fetch-build:/robs-fetch ./build_result
   cd build_result
   echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
   npm version --no-git-tag-version "${TRAVIS_TAG}"
-  npm publish
-else
-  echo "No publish required"
+  if [ ${TRAVIS} ]
+  then
+    npm publish
+  fi
 fi
